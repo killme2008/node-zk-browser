ce00eaf63c8fe0ff7ba1afe42f9d7d1f5b815b0c
dennis <dennis@dennis-laptop.(none)>
2011-06-02 20:59:24 +0800

    first commit

---
+++ REAME.md
@@ -0,0 +1,7 @@
+##node-zk-browser
+
+A zookeeper web administrator in node.js. It's based on express and node-zookeeper.
+
+##Run
+
+        node app.js
---
+++ app.js
@@ -0,0 +1,38 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d", app.address().port);
