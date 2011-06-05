774f9352e61931271ec5714a2c76d4b93795fe49
dennis <dennis@dennis-laptop.(none)>
2011-06-05 21:51:48 +0800

    Add login function

--- app.js
+++ app.js
@@ -4,20 +4,24 @@
 
 var express = require('express');
 var path=require('path');
+var fs=require('fs');
 var util=require('util');
 var ZkClient=require('./zk.js').ZkClient;
 var zkclient = new ZkClient("localhost:2181");
 
+var users = JSON.parse(fs.readFileSync(path.join(__dirname,'user.json'), 'utf8'));
 var app = module.exports = express.createServer();
 
 // Configuration
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: "node zk browser" }));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
@@ -32,8 +36,7 @@ app.configure('production', function(){
 
 //index
 app.get('/', function(req, res){
-  res.render('index', {
-  });
+    res.render('index', { });
 });
 //display tree
 app.get('/tree', function(req, res){
@@ -41,13 +44,20 @@ app.get('/tree', function(req, res){
     res.render('tree', {layout:false,'path':path  });
 });
 
+app.post("/login",function(req,res){
+    var user=req.body.user;
+    if(users[user.name]==user.password){
+        req.session.user=user.name
+    }
+    res.redirect("/get");
+});
 //query data
 app.get("/get",function(req,res){
     var path=req.query.path || "/";
     zkclient.zk.a_get(path,null,function(rc,err,stat,data){
         if(rc!=0)
             throw new Error(err);
-        res.render("data",{ layout: false, 'stat':stat,'data':util.inspect(data),'path':path});
+        res.render("data",{ layout: false, 'stat':stat,'data':data,'path':path,'user': req.session.user});
     });
 });
 //query children
